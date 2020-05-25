40b0bc9201f8159ea9d5640227f5496ddcfe4aae
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-05-25 11:05:23 +0100

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Emil Vaklinov on 25/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
