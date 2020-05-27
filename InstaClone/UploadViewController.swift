4e4f06c9b186732ab85dc8f3a518afc3c9090276
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-05-27 12:58:20 +0100

    saving data done

--- InstaClone/UploadViewController.swift
+++ InstaClone/UploadViewController.swift
@@ -68,11 +68,16 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                             
                             let firestoreDatabase = Firestore.firestore()
                             var firestoreReference : DocumentReference? = nil
-                            let firestorePost = ["imageUrl": imageUrl!, "postedBy": Auth.auth().currentUser!.email!, "postComment": self.commentText.text!, "date" : "date", "likes": 0] as [String : Any]
+                            let firestorePost = ["imageUrl": imageUrl!, "postedBy": Auth.auth().currentUser!.email!, "postComment": self.commentText.text!, "date" : FieldValue.serverTimestamp(), "likes": 0] as [String : Any]
                             firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
                                 if error != nil {
                                     
                                     self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                                } else {
+                                    // Saving data and opening the 0 index of the tapbar
+                                    self.imageView.image = UIImage(named: "select.png")
+                                    self.commentText.text = ""
+                                    self.tabBarController?.selectedIndex = 0
                                 }
                             })
                         }
